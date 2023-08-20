83322c30920bfeec9091e04e694c83501f76e591
aagaev <agaeana@gmail.com>
2023-08-20 22:02:09 +0400

    feat: implement request to Marvel Unlimited API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+
 import './style/style.scss';
+// import MarvelService from './services/MarvelService';
+
+
+// const marvel = new MarvelService();
+//  marvel.getAllCharacters().then(res => console.log(res))
+
+// marvel.getCharacter(1011052).then(res => console.log(res))
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/services/MarvelService.js
@@ -0,0 +1,43 @@
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=1d72cee4813314a40e4eb983489bd6b7';
+    getResource = async (url) => {
+        const res = await fetch(url);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status - ${res.status}`)
+        }
+
+        return await res.json()
+    }
+
+    getAllCharacters = async () => {
+        //return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res =  await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        console.log(res)
+        return res.data.results.map(this._transformCharacter)// передаем в map callback функцию которая будет трансформировать приходящие к ней элементы(айтемы). То есть будет переходить в методе map каждый отдельный объект по порядку  и уже будет возвращаться новый объект, а в конце получим новый массив с объектами 
+        //return res.data.results.map(item => this._transformCharacter(item))
+    }
+
+    getCharacter = async (id) => {
+        //return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    noDescriptionMessage = (str) => str ? `${str.slice(0, 60)}...`  : str = 'Sorry but there is no description for this character. Please check Wiki page';
+
+    _transformCharacter = (char) => {
+        return { 
+            name: char.name,
+            description: this.noDescriptionMessage(char.description),
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+
+}
+
+export default MarvelService;
