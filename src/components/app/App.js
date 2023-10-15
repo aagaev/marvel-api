bf62064460d6d075ff3df8ede5af7c0ec2e09ae1
aagaev <agaeana@gmail.com>
2023-10-15 16:29:28 +0400

    refactor: rename vars for better readability

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,34 @@
 
+import { lazy, Suspense } from 'react';
 import {BrowserRouter as Router,  Route, Routes} from 'react-router-dom';
 
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
+import { ComicsPage, SingleComicPage } from '../pages';//именнованный import из index.js
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+//import Page404 from '../pages/404' //для React.Lazy нам нужен именно дэфолтный экспорт из самого файла, а не именнованный из index.js
+//динамические импорты должны импортироваться после статических
+
+const Page404 = lazy(() => import('../pages/404'))
+const MainPage = lazy(() => import('../pages/MainPage'))
+// const ComicsPage = lazy(() => import('../pages/ComicsPage'))
+// const SingleComicPage = lazy(() => import('../pages/SingleComicPage'))
 
 const App = () =>  {
-    
-
-
+ 
     return (
         <Router>
             <div className="app">
             <AppHeader/>
-            <main>
-                <Routes>
-                    <Route path='/' element={ <MainPage/>}/>
-                    <Route path='/comics' element={ <ComicsPage/>}/> 
-                    <Route path='/comics/:comicId' element={ <SingleComicPage/>}/>   
-                    <Route path='*' element={<Page404/>}/>   
-
-                </Routes>
+            <main> 
+                <Suspense fallback={<Spinner/>}> 
+                    <Routes>
+                        <Route path='/' element={ <MainPage/>}/>
+                        <Route path='/comics' element={ <ComicsPage/>}/> 
+                        <Route path='/comics/:comicId' element={ <SingleComicPage/>}/>   
+                        <Route path='*' element={<Page404/>}/>   
+                    </Routes>
+                </Suspense>
             </main>
         </div>
         </Router>
