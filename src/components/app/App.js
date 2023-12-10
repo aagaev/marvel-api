6b715d8d477c05c0f258bf1d9a90b7a70a4fccb0
aagaev <agaeana@gmail.com>
2023-12-10 22:47:26 +0400

    feat: implement search form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -13,7 +13,7 @@ const Page404 = lazy(() => import('../pages/404'))
 const MainPage = lazy(() => import('../pages/MainPage'))
 // const ComicsPage = lazy(() => import('../pages/ComicsPage'))
 // const SingleComicPage = lazy(() => import('../pages/SingleComicPage'))
-
+ 
 const App = () =>  {
  
     return (
